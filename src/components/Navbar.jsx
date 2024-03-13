519dd3bad242df5c5ecb1569b932cadabc1215f7
imlnr <imlnr2002@gmail.com>
2024-03-13 19:17:22 +0530

    final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-
+import React from 'react';
+import logo from '../assets/Logo.png'
 const Navbar = () => {
     return (
-        <div className='w-full border-solid border-2 border-sky-500'>
-            <nav className='m-auto border-solid border-2 border-sky-500 h-10 w-11/12'>
-                border
+        <div className="p-4 w-full">
+            <nav className="h-10 w-11/12 m-auto container mx-auto flex justify-between items-center">
+                {/* Logo */}
+                <div className="text-white font-bold"><img src={logo} alt="" /></div>
+
+                {/* Navigation Items */}
+                <div className="flex items-center space-x-16">
+                    <a href="#" >Home</a>
+                    <a href="#" >Service</a>
+                    <a href="#" >Feature</a>
+                    <a href="#" >Product</a>
+                    <a href="#" >Testimonial</a>
+                    <a href="#">FAQ</a>
+                </div>
+
+                {/* Login and Signup Buttons */}
+                <div className="flex items-center space-x-8">
+                    <button className='rounded-md' >Login</button>
+                    <button className='bg-[#4CAF4F] text-white px-3 py-1 rounded-md'>Signup</button>
+                </div>
             </nav>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
