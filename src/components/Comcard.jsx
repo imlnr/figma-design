15ea0e07a243040cbfff2eec3fdd85db4876a1a5
imlnr <imlnr2002@gmail.com>
2024-03-17 00:12:23 +0530

    final commit

--- src/components/Comcard.jsx
+++ src/components/Comcard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Comcard = ({ val, head, para }) => {
     return (
-        <div className='w-full md:w-auto shadow-lg flex flex-col items-center justify-center px-6 py-4 gap-3 text-center'>
+        <div className='w-full md:w-auto shadow-lg flex flex-col items-center rounded justify-center px-6 py-4 gap-3 text-center'>
             <img src={val} alt={val} className='max-w-full' />
             <h1 className='text-xl md:text-2xl font-semibold'>{head}</h1>
             <p>{para}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,11 +14,12 @@ import icon3 from "../assets/Icon (2).png";
 
 import Clientlogo from '../components/Clientlogo';
 import Comcard from '../components/Comcard';
+import pass from '../assets/rafiki.png'
 
 const Home = () => {
     return (
-        <div className="bg-[#F5F7FA] min-h-screen">
-            <div className="flex flex-col items-center justify-around py-10 md:py-20">
+        <div className="min-h-screen">
+            <div className="flex flex-col items-center justify-around py-10 md:py-20 bg-[#F5F7FA]">
                 <div className='w-10/12 flex flex-col-reverse md:flex-row items-center justify-between px-4 md:px-0'>
                     <div className='flex flex-col gap-3 items-center md:items-start text-center md:text-left'>
                         <h1 className='font-inter text-5xl md:text-7xl font-semibold leading-tight md:leading-snug tracking-normal'>
@@ -61,6 +62,27 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            {/* <div className='mt-7'>
+                <div className='w-9/12 m-auto flex items-center justify-between gap-10'>
+                    <img src={pass} alt="" />
+                    <div className='w-1/2'>
+                        <h1 className='text-4xl md:text-4xl font-semibold mb-5'>The unseen of spending three years at Pixelgrade</h1>
+                        <p>When joining the JustGo Community, your success becomes our success. In other words, our Customer Success team will be on hand from day one to ensure we understand your requirements, share your goals and work with you for a smooth adoption of our software.</p>
+                        <button className='bg-[#4CAF4F] text-white px-5 py-2 mt-3 rounded-md'>Learn More</button>
+                    </div>
+                </div>
+            </div> */}
+            <div className='mt-7'>
+    <div className='w-full md:w-9/12 lg:w-10/12 xl:w-9/12 mx-auto flex flex-col lg:flex-row items-center justify-between gap-10'>
+        <img src={pass} alt="" className='' />
+        <div className='w-full xl:w-1/2 sm:11/12 p-6 md:w-11/12'>
+            <h1 className='text-2xl md:text-4xl font-semibold mb-5'>The unseen of spending three years at Pixelgrade</h1>
+            <p className='text-sm md:text-base'>When joining the JustGo Community, your success becomes our success. In other words, our Customer Success team will be on hand from day one to ensure we understand your requirements, share your goals and work with you for a smooth adoption of our software.</p>
+            <button className='bg-[#4CAF4F] text-white px-5 py-2 mt-3 rounded-md'>Learn More</button>
+        </div>
+    </div>
+</div>
+
         </div>
     );
 };
