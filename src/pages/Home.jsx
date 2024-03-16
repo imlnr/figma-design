e413b939029d8bf2b8c9d620bb0dc7905c263385
imlnr <imlnr2002@gmail.com>
2024-03-16 15:13:30 +0530

    final commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import illu from '../assets/Illustration.png'
 const Home = () => {
     return (
-        <div className="hero">
-            <div></div>
-            <div></div>
+        <div className="flex w-full items-center h-100 justify-center border-solid border-2 border-sky-500" style={{ height: "600px" }}>
+            <div className='w-11/12 flex items-center justify-around'>
+                <div className=''>
+                    <h1 className='font-inter text-8xl font-semibold leading-96 tracking-normal text-left'>Lessons and insights</h1>
+                    <h1 className='font-inter text-8xl font-semibold leading-96 tracking-normal text-left'>from 8 years</h1>
+                    <p>Where to grow your business as a photographer: site or social media?</p>
+                    <button>Register</button>
+                </div>
+                <div className=''>
+                    <img src={illu} alt="illustration" />
+                </div>
+            </div>
         </div>
     )
 }
