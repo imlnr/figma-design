5dffa32b4583b8ec82054daed47f7727533a68d0
imlnr <imlnr2002@gmail.com>
2024-03-17 13:01:30 +0530

    final commit|

--- src/components/Mcep.jsx
+++ src/components/Mcep.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 const Mcep = ({ img, num, type }) => {
     return (
         <div className='grid grid-cols-2'>
-            <img className='m-auto h-12 w-12' src={img} alt={img} />
-            <div>
-                <h2 className='text-2xl md:text-3xl font-semibold'>{num}</h2>
-                <p className='text-md'>{type}</p>
+            <img className='m-auto h-8 md:h-12 w-8 md:w-12' src={img} alt={img} />
+            <div className='m-0 p-0'>
+                <h2 className='text-lg md:text-3xl font-semibold'>{num}</h2>
+                <p className='text-sm md:text-md'>{type}</p>
             </div>
         </div>
+
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ import Mcep from '../components/Mcep';
 
 const Home = () => {
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen w-full">
             <div className="flex flex-col items-center justify-around py-10 md:py-20 bg-[#F5F7FA]">
                 <div className='w-10/12 flex flex-col-reverse md:flex-row items-center justify-between px-4 md:px-0'>
                     <div className='flex flex-col gap-3 items-center md:items-start text-center md:text-left'>
@@ -69,17 +69,8 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            {/* <div className='mt-7'>
-                <div className='w-9/12 m-auto flex items-center justify-between gap-10'>
-                    <img src={pass} alt="" />
-                    <div className='w-1/2'>
-                        <h1 className='text-4xl md:text-4xl font-semibold mb-5'>The unseen of spending three years at Pixelgrade</h1>
-                        <p>When joining the JustGo Community, your success becomes our success. In other words, our Customer Success team will be on hand from day one to ensure we understand your requirements, share your goals and work with you for a smooth adoption of our software.</p>
-                        <button className='bg-[#4CAF4F] text-white px-5 py-2 mt-3 rounded-md'>Learn More</button>
-                    </div>
-                </div>
-            </div> */}
-            <div className='mt-7 py-8'>
+
+            <div className='mt-7 py-8 '>
                 <div className='w-full md:w-9/12 lg:w-10/12 xl:w-9/12 mx-auto flex flex-col lg:flex-row items-center justify-between gap-10'>
                     <img src={pass} alt="" />
                     <div className='w-full xl:w-1/2 sm:11/12 p-6 md:w-11/12'>
@@ -91,20 +82,21 @@ const Home = () => {
             </div>
 
             <div className='w-full bg-[#F5F7FA] h-72 flex items-center justify-center'>
-                <div className='w-10/12 border-dotted border-2 border-sky-500 flex justify-around'>
-                    <div className='border-dotted border-2 border-sky-500 self-center'>
-                        <h3>Helping a local</h3>
-                        <h3>business reinvent itself</h3>
-                        <p>We reached here with our hard work and dedication</p>
+                <div className='w-11/12 md:w-3/4 lg:w-2/3 border-dotted border-2 border-sky-500 flex flex-col md:flex-row justify-around'>
+                    <div className='border-dotted border-2 border-sky-500 self-center mb-5 md:mb-0 md:mr-5'>
+                        <h3 className='text-2xl md:text-4xl font-semibold'>Helping a local</h3>
+                        <h3 className='text-2xl md:text-4xl font-semibold text-[#4CAF4F]'>business reinvent itself</h3>
+                        <p className='text-sm'>We reached here with our hard work and dedication</p>
                     </div>
                     <div className='grid grid-cols-2 gap-5 border-dotted border-2 border-sky-500'>
-                        <Mcep img={icon4} num={"2,245,341"} type={"Members"}/>
-                        <Mcep img={icon5} num={"46,328"} type={"Clubs"}/>
-                        <Mcep img={icon6} num={"828,867"} type={"Event Bookings"}/>
-                        <Mcep img={icon7} num={"1,926,436"} type={"Payments"}/>
+                        <Mcep img={icon4} num={"2,245,341"} type={"Members"} />
+                        <Mcep img={icon5} num={"46,328"} type={"Clubs"} />
+                        <Mcep img={icon6} num={"828,867"} type={"Event Bookings"} />
+                        <Mcep img={icon7} num={"1,926,436"} type={"Payments"} />
                     </div>
                 </div>
             </div>
+
 
             <div className='mt-7'>
                 <div className='w-full md:w-9/12 lg:w-10/12 xl:w-9/12 mx-auto flex flex-col lg:flex-row items-center justify-between gap-10'>
